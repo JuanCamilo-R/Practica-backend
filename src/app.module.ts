f8fe8c4ab2808d4830f260b71b3733924778caf7
JuanCamilo-R <51805884+JuanCamilo-R@users.noreply.github.com>
2021-07-12 11:41:07 -0500

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,45 @@
+import { Module, HttpModule, HttpService } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
+import { environments } from './environments';
+import config from './config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+      load: [config],
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+    }),
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/users/dtos/customer.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The name of the user' })
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  readonly phone: string;
+}
+
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
